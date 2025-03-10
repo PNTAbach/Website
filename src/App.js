b055bc78502ac3409a08a0f08c44c77d53af2ef5
Bruno Laizans <bumbalaizans@gmail.com>
2025-03-10 19:07:13 +0100

    Website MVP

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+    <Router basename='/' >
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/emailForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/emailForm.css';
+import Subscribe from './subscribe';
+
+const EmailForm = () => {
+
+  
+    return (
+        <div className="emailSection py-5 ">
+            <div className="email-form-container ">
+                <Subscribe/>
+            </div>
+        </div>
+    );
+  };
+  
+  export default EmailForm;
